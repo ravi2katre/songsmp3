19be7241987dff98e9a00a9b284c03ae55627b28
ravi <ravi2katre@gmail.com>
2017-07-26 16:59:45 +0530

    tiu 1

--- application/modules/admin/views/fileupload/edit.php
+++ application/modules/admin/views/fileupload/edit.php
@@ -11,19 +11,26 @@
                 <?php echo $form->open(); ?>
 
                 <?php
+                $title =  $this->input->post('title');
+                $artist =  $this->input->post('artist');
+                $album =  $this->input->post('album');
+                $year =  $this->input->post('year');
+                $comment =  $this->input->post('comment');
+
                 //cidb($mp3info['tags_html']);exit;
                 foreach ($mp3info['tags']['id3v1'] as $key => $val) {
-                    //cidb($val);
                     $value = implode(",", $val);
-                    echo $form->bs3_text($key, $key, $value);
+                    $$key= $value;
                 }
+
+                echo $form->bs3_text('title', 'title', $title);
+                echo $form->bs3_text('artist', 'artist', $artist);
+                echo $form->bs3_text('album', 'album', $album);
+                echo $form->bs3_text('year', 'year', $year);
+                echo $form->bs3_text('comment', 'comment',$comment);
+
                 ?>
-                <?php /* echo $form->bs3_text('First Name', 'first_name'); ?>
-                <?php echo $form->bs3_text('Last Name', 'last_name'); ?>
-                <?php echo $form->bs3_text('Username', 'username'); ?>
-                <?php echo $form->bs3_text('Email', 'email'); ?>
-                <?php echo $form->bs3_password('Password', 'password'); ?>
-                <?php echo $form->bs3_password('Retype Password', 'retype_password'); */ ?>
+
                 <?php echo $form->bs3_submit(); ?>
 
                 <?php echo $form->close(); ?>
